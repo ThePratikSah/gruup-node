3d9803d29b444f8c5a07685bffe7d3e51a3512fb
Pratik Sah <shivam3448@gmail.com>
2025-01-19 02:13:12 +0530

    Added validator for new restaurant creation

--- src/routes/restaurants.ts
+++ src/routes/restaurants.ts
@@ -3,8 +3,14 @@ import {
   addMenuItemToRestaurant,
   addNewRestaurant,
 } from "../controllers/restaurants";
+import { validateData } from "../middleware/dataValidator";
+import { addNewRestaurantSchema } from "../validator/validator";
 
 export const restaurantRouter = Router();
 
-restaurantRouter.post("/", addNewRestaurant);
+restaurantRouter.post(
+  "/",
+  validateData(addNewRestaurantSchema, "body"),
+  addNewRestaurant
+);
 restaurantRouter.post("/add-menu-item-to-restaurant", addMenuItemToRestaurant);
