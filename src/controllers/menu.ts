5c897341432a0eb5c6830de6c8b72a69c867b2a9
Pratik Sah <shivam3448@gmail.com>
2025-01-18 15:52:12 +0530

    Added option for bulk menu item

--- src/controllers/menu.ts
+++ src/controllers/menu.ts
@@ -1,5 +1,9 @@
 import { Request, Response } from "express";
-import { createNewMenuItem, getRestaurantData } from "../helpers/query";
+import {
+  bulkCreateMenuItem,
+  createNewMenuItem,
+  getRestaurantData,
+} from "../helpers/query";
 import { Prisma } from "@prisma/client";
 
 export async function menuController(req: Request, res: Response) {
@@ -26,5 +30,19 @@ export async function createMenuItem(req: Request, res: Response) {
     return res
       .status(500)
       .json({ error, message: "Failed to create menu item" });
+  }
+}
+
+export async function createBulkMenuItem(req: Request, res: Response) {
+  try {
+    // TODO: validate data
+    const menuItemPayload = req.body as Prisma.MenuItemUncheckedCreateInput[];
+    const menuItems = await bulkCreateMenuItem(menuItemPayload);
+
+    return res.json({ message: "Bulk menu item created", menuItems });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ error, message: "Failed to create bulk menu item" });
   }
 }
--- src/routes/menu.ts
+++ src/routes/menu.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
-import { menuController, createMenuItem } from "../controllers/menu";
+import {
+  menuController,
+  createMenuItem,
+  createBulkMenuItem,
+} from "../controllers/menu";
 import { validateData } from "../middleware/menu";
 import { restaurantSchema } from "../validator/restaurant";
 
@@ -7,3 +11,4 @@ export const menuRouter = Router();
 
 menuRouter.get("/:shortname", validateData(restaurantSchema), menuController);
 menuRouter.post("/menu-item", createMenuItem);
+menuRouter.post("/bulk-menu-item", createBulkMenuItem);
